f6b8e19d294324153524e31242feac074aad5beb
abn <ikomangbramantara@gmail.com>
2025-07-04 11:13:19 +0800

    Update proxy.js

--- page/api/proxy.js
+++ page/api/proxy.js
@@ -1,23 +1,15 @@
 export default async function handler(req, res) {
   const { url } = req.query;
 
-  if (!url) {
-    return res.status(400).json({ error: "URL kosong bro!" });
-  }
+  if (!url) return res.status(400).send("URL kosong!");
 
   try {
-    const response = await fetch(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (SafeProxyBot)',
-        'Referer': ''
-      }
-    });
-
+    const response = await fetch(url);
     const html = await response.text();
+
     res.setHeader("Content-Type", "text/html");
     res.send(html);
-
   } catch (err) {
-    res.status(500).json({ error: "Gagal ambil halaman: " + err.message });
+    res.status(500).send("Gagal ambil halaman: " + err.message);
   }
 }
